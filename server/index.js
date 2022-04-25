bbdcc5a20d649d9fc78d897a47c79c86a9f38ef5
deeppatel007 <deepp0254@gmail.com>
2022-04-25 14:55:05 +0530

    heroku

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 app.use('/api',router);
+const port  = process.env.PORT|| 4000; 
 
 // const username=process.env.DB_USERNAME;
 // const password=process.env.DB_PASSWORD;
